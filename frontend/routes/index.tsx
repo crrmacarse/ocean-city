51e63f9319265914783ceb69e22324f12dfe55a2
crrmacarse <crrmacarse@gmail.com>
2020-08-08 11:20:11 +0800

    Fix unremoved code

--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from 'components/app';
-import Validate from 'components/validate';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={App} />
-    <Route exact path="/authorize/:code" component={Validate} />
     <Route component={() => <h1>Missing</h1>} />
   </Switch>
 );
